890697954a797a18754ff90d731ad87fa6a0e1e2
Francis Lloyd Cantones <frnsllydndrew@gmail.com>
2026-06-30 09:20:29 +0800

    upgrading the fonts

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Preconnect for Inter font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+
     <link rel="stylesheet"
       href="https://unpkg.com/aos@2.3.4/dist/aos.css">
 
